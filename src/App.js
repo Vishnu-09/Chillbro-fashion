d813bfce4be55a8d7a3eafb8032119368f31003a
Vishnu-09 <115658884+Vishnu-09@users.noreply.github.com>
2023-11-17 12:46:00 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,272 @@
+import React from 'react';
+import './App.css'; 
+
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <div id="header">
+        <h4 id="new">
+      <img src="chillbro1.png" alt="new" style={{ width: '85px', height: 'auto' }} />
+    </h4>
+          <div className="header-list">
+            <nav className="header-list-nav">
+              <ul>
+                <li><a className="act" href="index.html">Home</a></li>
+                <li><a href="#Shop">Shop</a></li>
+                <li><a href="#Cart">Cart</a></li>
+                <li><a href="signin.html">Signin</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <section id="hero">
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <center> <img src="https://www.snitch.co.in/cdn/shop/files/yellow-buy-1-get-1-free-banner-web-_1.webp?v=1698996561&width=1600" alt="nw" style={{ margin: '20px' }} width="1400" /></center>
+          <img src="https://getketchadmin.getketch.com/banner/1691157544TOKYO%20TALKIES%20WEB%20BANNER.webp" alt='new' />
+          <br /><br /><br />
+          <div style={{ textAlign: 'center' }}>
+            <strong><h1>✨DIWALI PRICE SLASH✨</h1></strong>
+          </div>
+          <pre style={{ textAlign: 'center' }}><h2>LIMITED PERIOD OFFER</h2></pre>
+        </section>
+
+        <main>
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <center id="Shop"><br /><br /><br /><br /><br /><p style={{ textAlign: 'center', fontSize: '24px', color: '#0077B6', backgroundColor: '#F4A261', padding: '10px 20px', borderRadius: '5px', display: 'inline-block', cursor: 'pointer' }}>SHOP</p></center>
+
+          <section id="features" className="section-p1">
+          <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/4MSCR7249-02-M17.jpg?v=1689789669" alt="free shipping" />
+    <h6>Plateau Green Shirt</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>599</h3>
+    <b>Save 19%</b><b />
+    
+
+</div>
+
+            <div className="f-box">
+              <img src="https://www.snitch.co.in/cdn/shop/files/4MSS1900-02-M10619.jpg?v=1686640743" alt="online order" />
+              <h6>Printed White Shirt</h6><br /><br />
+              <h4><strike>Rs 999</strike></h4>
+              <h3>599</h3>
+              <b>Save 19%</b>
+            </div>
+            <div className="f-box">
+              <img src="https://www.snitch.co.in/cdn/shop/files/4MSS2189-02-M25.jpg?v=1689829892" alt="save money" />
+              <h6>Printed Black Shirt</h6><br /><br />
+              <h4><strike>Rs 999</strike></h4>
+              <h3>599</h3>
+              <b>Save 19%</b>
+            </div>
+            <div className="f-box">
+              <img src="https://cdn.shopify.com/s/files/1/0420/7073/7058/files/4MST2172-01-M32_d67d245f-1ac0-405c-8fd9-34b94ab1719f.jpg?v=1691061465&width=200" alt="promotions" />
+              <h6>Plateau Design Shirt</h6><br /><br />
+              <h4><strike>Rs 999</strike></h4>
+              <h3>599</h3>
+              <b>Save 19%</b>
+            </div>
+            <div className="f-box">
+              <img src="https://cdn.shopify.com/s/files/1/0420/7073/7058/files/4MSS2129-11-M28.jpg?v=1698131436&width=200" alt="happy sell" />
+              <h6>Man-ly Pink Shirt</h6><br /><br />
+              <h4><strike>Rs 999</strike></h4>
+              <h3>599</h3>
+              <b>Save 19%</b>
+            </div>
+            <div className="f-box">
+              <img src="https://cdn.shopify.com/s/files/1/0420/7073/7058/files/4MSS2129-02-M32.jpg?v=1691069574&width=300" alt="24/7 support" />
+              <h6>Casual Shirt</h6><br /><br />
+              <h4><strike>Rs 999</strike></h4>
+              <h3>599</h3>
+              <b>Save 19%</b>
+            </div>
+          </section>
+
+<div><div></div></div>
+<br /><br />
+<div style={{ textAlign: 'center' }}>
+            <strong><h1>✨NEW ARRIVALS✨</h1></strong>
+          </div>
+        
+       
+
+
+<section id="featuress" className="section-p1">
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/4MSS2451-04-M45.webp?v=1699357095&width=1800" alt="free shipping" />
+    <h6>Plateau Green Shirt</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>599</h3>
+    <b>Save 19%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://cdn.shopify.com/s/files/1/0420/7073/7058/files/4MSW9015-02-M37.webp?v=1699531542&width=300" alt="online order" />
+    <h6>Printed White Shirt</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>599</h3>
+    <b>Save 19%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/products/Amin4174.jpg?v=1672146549" alt="save money" />
+    <h6>Printed Black Shirt</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>599</h3>
+    <b>Save 19%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/4MSS2517-03-M16.webp?v=1699552284&width=1800" alt="promotions" />
+    <h6>Plateau Design Shirt</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>599</h3>
+    <b>Save 19%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/4MSS2517-19-M35.webp?v=1699553639" alt="happy sell" />
+    <h6>Man-ly Pink Shirt</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>599</h3>
+    <b>Save 19%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://cdn.shopify.com/s/files/1/0420/7073/7058/files/4MSS2129-02-M32.jpg?v=1691069574&width=300" alt="24/7 support" />
+    <h6>Casual Shirt</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>599</h3>
+    <b>Save 19%</b>
+  </div>
+</section>
+<center><button><a id="new1" href="index1.html" target='new'>Explore</a></button></center>
+
+
+<div></div><br />
+<div style={{ textAlign: 'center' }}>
+            <strong><h1>✨CHILLING SHADES FOR YOU✨</h1></strong>
+          </div><br /><br />
+          <section id="features1" className="section-p1">
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/IMG_0436.jpg?v=1698221140&width=1800" alt="free shipping" />
+    <h6>Square Orange shade</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>399</h3>
+    <b>Save 29%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/IMG_0397.jpg?v=1698221497&width=1800" alt="online order" />
+    <h6>Orbital Aura</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>399</h3>
+    <b>Save 29%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/IMG_0170.jpg?v=1698221327&width=1080" alt="save money" />
+    <h6>Recta Luxe Shade</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>399</h3>
+    <b>Save 29%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/IMG_0113.jpg?v=1698122061&width=1800"alt='bgvcx' />
+   <h6>Square chase shade</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>399</h3>
+    <b>Save 29%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/IMG_0235.jpg?v=1698221182&width=1800" alt='cvbn'/>
+    <h6>Modern Block Sunglasses</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>399</h3>
+    <b>Save 29%</b>
+  </div>
+  <div className="f-box">
+    <img src="https://www.snitch.co.in/cdn/shop/files/IMG_0199.jpg?v=1698221128&width=1080" alt="24/7 support" />
+    <h6>Circle Vintage Shade</h6><br /><br />
+    <h4><strike>Rs 999</strike></h4>
+    <h3>399</h3>
+    <b>Save 29%</b>
+  </div>
+
+</section>
+
+        <section id="off-banner" className="section-m1">
+            
+            
+          </section>
+
+          <section id="banners" className="section-p1">
+            <div className="big-banners">
+              <div className="big-banners-1">
+              
+                <h2>You can have anything</h2>
+                <span><h3>You want in life if </h3></span>
+                <h2>You Dress for it</h2>
+              </div>
+              <div className="big-banners-2">
+                <h2>Clothes mean nothing</h2>
+                <h4>until Someone</h4>
+                <h2>lives in them</h2>
+              </div>
+            </div>
+          </section>
+
+          <section id="newsletter">
+            <div className="newsletter-text">
+              <h3>Sign Up For New Offers</h3>
+              <h5>get e-mail updates about out latest shop and <span>special offers</span></h5>
+            </div>
+            <div className="form">
+              <input type="email" placeholder="Your email address" id="email-address-input" />
+              <button>Sign Up</button>
+            </div>
+          </section>
+        </main>
+
+        <footer>
+          <div id="footer">
+            <div className="contact">
+              <a href="indext.html"><img src="images/logo.png" alt="" /></a>
+              <br /><br /><br />
+              <h3 id="contact">Contact</h3>
+              <address>
+                <p><b>Address:</b> Shridevi Engineering College</p>
+                <p><b>Phone:</b> 9628545475</p>
+                <p><b>Hours</b> 9:00 - 17:00. Mon - Sat</p>
+              </address>
+              
+              <br />
+            </div>
+         
+            <div className="myaccount ">
+              <h3>My account</h3>
+              <br />
+              <a href="signin.html" target="">Sign In</a>
+              <a href="v">View Cart</a>
+             
+              <a href="c">Help</a>
+            </div>
+            <div className="install">
+              <h3>Install App</h3>
+              <br />
+              <p>From App Store or Google Play</p>
+              <p>Secured Payment Gateways</p>
+              
+            </div>
+          </div>
+        </footer>
+      </main>
+    </div>
+  );
+};
+
+
+export default App;
+
+
+
+
